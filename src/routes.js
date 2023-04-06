34df76202ec2ea8d6ecc533653708b6e074d835d
ka-raph <rkarampournis@gmail.com>
2023-04-06 11:16:59 +0200

    removed redundant path part

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 export default {
-    default: '/views/home.md',
-    fallback: '/views/404.md',
-    'installation': '/views/Installation/install.md',
-    'getstarted': '/views/Getting Started/getstarted.md',
+    default: '/home.md',
+    fallback: '/404.md',
+    'installation': '/Installation/install.md',
+    'getstarted': '/Getting Started/getstarted.md',
 }
