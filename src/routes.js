cb99eac1ea012c4e3ce615edbd9b06e26c84ab40
Raphael Karampournis <raphael.karampournis@lexisnexisrisk.com>
2023-07-21 13:53:21 +1000

    added missing routes

--- src/routes.js
+++ src/routes.js
@@ -3,4 +3,7 @@ export default {
     fallback: '/404.md',
     'installation': '/Installation/install.md',
     'getstarted': '/Getting Started/getStarted.md',
+    'usage': '/Usage/usage.md',
+    'configuration': '/Configuration/configuration.md',
+    'api': '/Api/api.md',
 }
